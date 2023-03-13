e7588f5f721ea9af630bc65b93c30da0781d7aba
Danielwinkelmann <dwinkelmann@outlook.com>
2023-03-13 15:28:22 +0100

    hotfix: 0.0.8

--- src/runtime/composables/useAccessToken.ts
+++ src/runtime/composables/useAccessToken.ts
@@ -1,8 +1,8 @@
 import { useCookie, useRuntimeConfig } from '#imports'
 
 export const useAccessToken = () => {
-  const {  cookieOptions } = useRuntimeConfig().public.auth
+  const { cookieOptions } = useRuntimeConfig().public.auth
   const cookieName = `${cookieOptions.name}-access-token`
 
-  return useCookie<string | null | undefined>(cookieName, { sameSite: true, maxAge: 3600, default: () => '' })
+  return useCookie<string | null | undefined>(cookieName, { sameSite: true, maxAge: 20 * 60, default: () => '' })
 }
--- src/runtime/composables/useRefreshToken.ts
+++ src/runtime/composables/useRefreshToken.ts
@@ -4,5 +4,5 @@ export const useRefreshToken = () => {
   const { auth: { cookieOptions } } = useRuntimeConfig().public
   const cookieName = `${cookieOptions.name}-refresh-token`
 
-  return useCookie<string | null | undefined>(cookieName, { sameSite: 'strict', default: () => '' })
+  return useCookie<string | null | undefined>(cookieName, { sameSite: 'strict', maxAge: 12 * 3600 * 7, default: () => '' })
 }
